0b3ff716726a65768d5b1a8816c330fae23b96b2
gnudeb <gnudeb0@gmail.com>
2018-05-17 15:28:08 +0300

    Couple Vue with backend

--- backend/static/spa.js
+++ backend/static/spa.js
@@ -1,9 +1,63 @@
-console.log(123);
 
 var app = new Vue({
   el: "#app",
   delimiters: ["[[", "]]"],
+
   data: {
-    msg: "Hello, SPA!"
-  }
+    categories: [],
+    products: [],
+    product: null,
+  },
+
+  methods: {
+    updateCategories() {
+      fetch("/api/categories/")
+        .then(response => response.json())
+        .then(json => {
+          this.categories = json.categories;
+        });
+    },
+
+    setCategory(category) {
+      fetch("/api/category/" + category + "/")
+        .then(response => response.json())
+        .then(json => {
+          if (!json.success) {
+            console.log(json.message);
+            return;
+          }
+          this.products = json.products;
+          this.product = null;
+        });
+    },
+
+    setProduct(id) {
+      fetch("/api/product/" + id + "/")
+        .then(response => response.json())
+        .then(json => {
+          if (!json.success) {
+            console.log(json.message);
+            return;
+          }
+          this.product = json.product;
+          this.issueClick(id);
+        });
+    },
+
+    issueClick(id) {
+      console.log("Click " + id);
+    },
+  },
+
+  beforeMount() {
+    this.updateCategories();
+  },
+
 })
+
+function POST(url, data) {
+  return fetch(url, {
+
+  })
+}
+
